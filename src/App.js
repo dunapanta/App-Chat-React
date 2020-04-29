3454f33492902fc51a3550118ccceec0196eedee
luis-rios <vvrhz11@gmail.com>
2020-04-28 22:13:45 -0500

    crenado-login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import {Home} from './views/Home/Home';
-
+import Login from './views/Autenticacion/InicioSesion';
 function App() {
   return (
     <div className="App">
       <Home />
+      <Login/>
     </div>
   );
 }
