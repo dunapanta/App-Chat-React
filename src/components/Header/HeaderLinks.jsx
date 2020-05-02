0191942feef32828c444f31cb663f29f13dcc982
dunapanta <dunapanta2@gmail.com>
2020-05-02 02:00:22 -0500

    Creando navbar y parallax de Home

---
+++ src/components/Header/HeaderLinks.jsx
@@ -0,0 +1,68 @@
+/*eslint-disable*/
+import React from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
+import IconButton from "@material-ui/core/IconButton";
+// react components for routing our app without refresh
+import { Link } from "react-router-dom";
+
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Tooltip from "@material-ui/core/Tooltip";
+
+// @material-ui/icons
+import { PersonAdd, VpnKeyRounded } from "@material-ui/icons";
+
+// core components
+import CustomDropdown from "components/CustomDropdown/CustomDropdown.jsx";
+import Button from "components/CustomButtons/Button.jsx";
+
+import headerLinksStyle from "assets/jss/material-kit-react/components/headerLinksStyle.jsx";
+
+function HeaderLinks({ ...props }) {
+  const { classes } = props;
+  return (
+    <List className={classes.list}>
+      <ListItem className={classes.listItem}>
+        <Button
+          href="https://github.com/dunapanta/App-Chat-React"
+          color="transparent"
+          target="_blank"
+          className={classes.navLink}
+        >
+          <PersonAdd className={classes.icons} /> REGISTRARSE
+        </Button>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Button
+          href="https://github.com/dunapanta/App-Chat-React"
+          color="transparent"
+          target="_blank"
+          className={classes.navLink}
+        >
+          <VpnKeyRounded className={classes.icons} /> INGRESAR
+        </Button>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Tooltip
+          id="github-tooltip"
+          title="Ver Repositorio de Github"
+          placement={window.innerWidth > 959 ? "top" : "left"}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            color="transparent"
+            href="https://github.com/dunapanta/App-Chat-React"
+            target="_blank"
+            className={classes.navLink}
+          >
+            <i className={classes.socialIcons + " fab fa-github"} />
+          </Button>
+        </Tooltip>
+      </ListItem>
+    </List>
+  );
+}
+
+export default withStyles(headerLinksStyle)(HeaderLinks);
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,27 +1,61 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
+import React, {Component} from "react";
+// nodejs library to set properties for components
+import PropTypes from "prop-types";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// react components for routing our app without refresh
+import { Link } from "react-router-dom";
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+// @material-ui/icons
+// core components
+import Header from "components/Header/Header.jsx";
+import Footer from "components/Footer/Footer.jsx";
+import GridContainer from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+import Button from "components/CustomButtons/Button.jsx";
+import Parallax from "components/Parallax/Parallax.jsx";
+// sections for this page
+import HeaderLinks from "components/Header/HeaderLinks.jsx";
 
-const Home = () => {
+import componentsStyle from "assets/jss/material-kit-react/views/components.jsx";
 
-    const useStyles = makeStyles((theme) => ({
-        root: {
-          '& > *': {
-            margin: theme.spacing(1),
-          },
-        },
-      }));
+class Home extends Component {
 
-        const classes = useStyles();
-        return(
-
-            <div className={classes.root}>
-                <Button variant="contained" color="primary">
-                    Primary
-                </Button>
-            </div>
+    render(){
+      const { classes, ...rest } = this.props;
+      return(
+        <div>
+            <Header
+              brand="App Chat React"
+              rightLinks={<HeaderLinks />}
+              fixed
+              color="transparent"
+              changeColorOnScroll={{
+                height: 400,
+                color: "white"
+              }}
+              {...rest}
+            />
+            <Parallax image={require("assets/img/parallax-img.jpg")}>
+              <div className={classes.container}>
+                <GridContainer>
+                  <GridItem>
+                    <div className={classes.brand}>
+                      <h1 className={classes.title}>Aplicación de Chat</h1>
+                      <h3 className={classes.subtitle}>
+                        Implementada con Firebase
+                      </h3>
+                    </div>
+                  </GridItem>
+                </GridContainer>
+              </div>
+            </Parallax>
+        </div>
         );
+    }
+    
     
 }
 
-export default Home;
+export default withStyles(componentsStyle)(Home);
