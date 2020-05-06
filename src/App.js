793da9e97db38c79ef74ec7190dde424e1ad2bf3
salomon <acaru.211@gmail.com>
2020-05-06 17:36:42 -0600

    registro

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import './App.css';
 import Home from './views/Home/Home';
+import Registre from './registro/Registrarse/register-component';
 //import Login from './views/Autenticacion/InicioSesion';
+import Register from 'views/Home/HomeSections/Home-register' ;
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  
+} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/home" exact >
+            <Home />
+          </Route>
+
+          <Route path="/registro">
+            <Register/>
+          </Route>
+          
+          <Route path="/Login">
+            <Login/>
+          </Route>
+
+          <Route path="/" exact >
+            <Home />
+          </Route>
+          </Switch >
+          
+       
+
+      </div>
+    
+    </Router>
+    
   );
 }
 
