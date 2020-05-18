6768c2ba086bbd2c9ecc0a8549a457512ceb89b1
luis-rios <vvrhz11@gmail.com>
2020-05-18 15:47:52 -0500

    'areglando_footer'

--- src/views/Chat/chat-component.jsx
+++ src/views/Chat/chat-component.jsx
@@ -5,17 +5,15 @@ import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import FormControl from '@material-ui/core/FormControl';
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
+
 import AccountCircle from '@material-ui/icons/AccountCircle';
 
 import Button from '@material-ui/core/Button';
 
-import DeleteIcon from '@material-ui/icons/Delete';
-import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+
 import KeyboardVoiceIcon from '@material-ui/icons/KeyboardVoice';
 import Icon from '@material-ui/core/Icon';
-import SaveIcon from '@material-ui/icons/Save';
+
 import './chat.css';
 import Monitor from '../../assets/img/login2.jpg';
 
@@ -44,7 +42,7 @@ export const Chat = () => {
         <Input
           id="input-with-icon-adornment"
           startAdornment={
-            <InputAdornment position="start">
+                <InputAdornment position="end" className="input">
               <AccountCircle />
             </InputAdornment>
           }
@@ -54,8 +52,7 @@ export const Chat = () => {
         variant="contained"
         color="primary"
         className={classes.button}
-        endIcon={<Icon>send</Icon>}
-        className="style"
+        endIcon={<Icon>send</Icon>}        
         position="end"
         
       >
