1c034e1d4a43f11ce55d3a3c0709a74bce78c1ef
dunapanta <dunapanta2@gmail.com>
2020-05-18 22:23:24 -0500

    Agregando comunicacion con base de datos

--- src/views/Home/HomeSections/Home-chat.js
+++ src/views/Home/HomeSections/Home-chat.js
@@ -2,10 +2,27 @@ import React from "react";
 import Chat from "../../Chat/chat-component";
 //import Sidebar from "../../sidebar/sidebar-chat-component";
 import ConversationList from 'componentes/ConversationList/index';
+import Button from '@material-ui/core/Button';
 import './estilo-chat.css';
 
 
 function HomeChat(props){
+
+    const mensaje = "prueba mensaje";
+
+    const handleUser = () => {
+        const mensaje = "prueba mensaje";
+        fetch('https://app-chat-react-a0d4a.firebaseio.com/mensajes.json', {
+            method: 'POST',
+            body: JSON.stringify(mensaje),
+            headers: { 'Content-Type': 'aplication/json' }
+        }).then(response => {
+            console.log("Clic boton");
+            return response.json();
+        }).catch(error => {
+            return console.log("Error en la PROMESA");
+        })
+    }
     return (
         <div className="estilo-chat">
             <div className='conver'>
@@ -14,8 +31,7 @@ function HomeChat(props){
             <div className='chat'>
                 <Chat  />
             </div>
-            
-           
+            <Button onClick={handleUser}>BOTON</Button>
         </div>    
     )
 }
